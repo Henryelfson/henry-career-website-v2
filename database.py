3a21ee31617d88ea2171139a244b46660ed844c2
Henryelfson <135180905+Henryelfson@users.noreply.github.com>
2025-08-26 22:03:07 +0000

    edited some basic stuffx

--- database.py
+++ database.py
@@ -2,6 +2,7 @@ from sqlalchemy import create_engine, text
 import os
 
 db_connection_string = os.environ['DB_CONNECTION_STRING']
+
 engine = create_engine(
     db_connection_string,
     connect_args={
@@ -12,10 +13,11 @@ engine = create_engine(
 )
 
 def load_jobs_from_db():
-  with engine.connect() as conn:
-    result = conn.execute(text("SELECT * FROM jobs"))
+    with engine.connect() as conn:
+        result = conn.execute(text("SELECT * FROM jobs"))
 
-  jobs = []
-  for row in result.all():
-    jobs.append(dict(row._mapping))
-  return jobs
+        jobs = []
+        for row in result.all():
+            jobs.append(dict(row._mapping))
+
+    return jobs
