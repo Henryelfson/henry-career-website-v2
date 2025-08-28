a99a9f7263519b07e43372e4ec28654114b0e1b6
Henryelfson <135180905+Henryelfson@users.noreply.github.com>
2025-08-28 16:06:43 +0000

    store application in database

--- database.py
+++ database.py
@@ -23,12 +23,29 @@ def load_jobs_from_db():
     return jobs
 
 def load_job_from_db(id):
-  with engine.connect() as conn:
-    result = conn.execute(
-      text("SELECT * FROM jobs WHERE id = :id"),
-      {"id": id}   
-    )
-    rows = result.all()
-    if len(rows) == 0:
-      return None
-    return dict(rows[0]._mapping)
+    with engine.connect() as conn:
+        result = conn.execute(
+            text("SELECT * FROM jobs WHERE id = :id"),
+            {"id": id}   
+        )
+        rows = result.all()
+        if len(rows) == 0:
+            return None
+        return dict(rows[0]._mapping)
+
+def add_application_to_db(job_id, data):
+    with engine.begin() as conn:  
+        query = text("""
+            INSERT INTO applications 
+            (job_id, full_name, email, linkedin_url, education, work_experience, resume_url) 
+            VALUES (:job_id, :full_name, :email, :linkedin_url, :education, :work_experience, :resume_url)
+        """)
+        conn.execute(query, {
+            "job_id": job_id,
+            "full_name": data.get('full_name'),
+            "email": data.get('email'),
+            "linkedin_url": data.get('linkedin_url'),
+            "education": data.get('education'),
+            "work_experience": data.get('work_experience'),
+            "resume_url": data.get('resume_url')
+        })
